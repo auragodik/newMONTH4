dfecf679d131c29c530fc3e2ece5ed47923c7a02
auragodik <nuradilkarybaev6@gmail.com>
2025-12-06 12:09:57 +0600

    month4 test

--- MyShop/views.py
+++ MyShop/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from .models import Category, Product
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
 def CategoryView(request):
     categories = Category.objects.all()
-    return render(request, "categroies.html", {"categories": categories})
+    return render(request, "MyShop/categories.html", {"categories": categories})
 
 def ProductView(request):
     products = Product.objects.all()
-    return render(request, "products.html", {"products": products})
+    return render(request, "MyShop/products.html", {"products": products})
 
 def CategoryProducts(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     products = Product.objects.filter(category=category)
-    return render(request, 'category_products.html', {
+    return render(request, 'MyShop/category_products.html', {
         "category": category,
         "products": products
     })
--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import AboutYou
 
+
 # Register your models here.
 @admin.register(AboutYou)
 class NewsAboutYouAdmin(admin.ModelAdmin):
